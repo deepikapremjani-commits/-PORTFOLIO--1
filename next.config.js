8614de02670f34006d3b3c0606c0e998e243d49a
deepikapremjani-commits <deepikapremjani@gmail.com>
2026-03-25 23:16:22 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  basePath: '/-PORTFOLIO--1',
+  assetPrefix: '/-PORTFOLIO--1',
+  images: {
+    unoptimized: true,
+  },
+}
 
 module.exports = nextConfig
