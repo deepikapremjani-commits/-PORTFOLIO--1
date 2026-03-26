937257b8f6ab6b920a4f389360ffb8c0abdc5c10
Deepikagit config --global user.email deepikapremjani@gmail.com <deepikapremjani@gmail.com>
2026-03-26 17:26:33 +0530

    fix vercel config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/-PORTFOLIO--1',
-  assetPrefix: '/-PORTFOLIO--1',
   images: {
     unoptimized: true,
   },
