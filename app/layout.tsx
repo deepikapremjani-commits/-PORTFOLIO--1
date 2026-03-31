1ba9cae25ff61cdb95fc74d51ac0b05fbcb67148
Deepikagit config --global user.email deepikapremjani@gmail.com <deepikapremjani@gmail.com>
2026-03-31 17:32:19 +0530

    add navbar to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from '../src/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Deepika Premjani | Interior Designer',
-  description: 'High-end Scrollytelling Personal Portfolio',
+  description: 'High-end Interior Design Portfolio',
 }
 
 export default function RootLayout({
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
